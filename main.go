7e73cdff6cc6138edd7fe1c242148982885fcb54
Nils Larsgård <nilsmagnus@gmail.com>
2017-04-08 21:52:41 +0200

    change from map to array of tickers

--- main.go
+++ main.go
@@ -1,40 +1,42 @@
 package main
 
 import (
-	"encoding/binary"
 	"encoding/json"
-	"encoding/xml"
 	"flag"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/nilsmagnus/grpc-samples/sample"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	show := flag.String("show", "nothing", "what to show: json, xml, proto, protolen")
+	show := flag.String("show", "nothing", "what to show: json, jsonlen, proto, protolen")
 	numberOfMapEntries := flag.Int("entries", 100, "How many entries in map")
 
 	flag.Parse()
 
-	tickers := make(map[string]float32)
+	tickers := make([]*sample.Ticker, *numberOfMapEntries)
 
-	for i := 1; i <= *numberOfMapEntries; i++ {
-		tickers[fmt.Sprintf("ticker-%d", i)] = 3.2
+	for i := 0; i < *numberOfMapEntries; i++ {
+		tickers[i] = &sample.Ticker{
+			Value: rand.Float32() * 10.0,
+			Name:  RandStringRunes(3),
+		}
 	}
 
 	protosome := &sample.Test{
 		Query:         "myQuery",
 		PageNumber:    42,
 		ResultPerPage: 100,
-		TickerValues:  tickers,
+		Tickers:       tickers,
 	}
 
 	switch *show {
 	case "protolen":
 		data, _ := proto.Marshal(protosome)
-
-		fmt.Printf("size of protoencoded : %d\n", binary.Size(data))
+		fmt.Printf("size of protoencoded : %d\n", len(data))
 	case "proto":
 		data, _ := proto.Marshal(protosome)
 		fmt.Println(data)
@@ -43,16 +45,23 @@ func main() {
 		fmt.Println(string(jsonified))
 	case "jsonlen":
 		jsonified, _ := json.Marshal(protosome)
-		fmt.Printf("json length(raw): %s", len(jsonified))
-	case "xml":
-		xmlified, _ := xml.Marshal(protosome)
-		fmt.Println(string(xmlified))
-	case "xmllen":
-		xmlified, _ := xml.Marshal(protosome)
-		fmt.Printf("xml lenght(raw):", len(xmlified))
-
+		fmt.Printf("json length(raw): %d\n", len(jsonified))
 	default:
 		fmt.Printf("unknown show-option: %s\n", *show)
 	}
 
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
